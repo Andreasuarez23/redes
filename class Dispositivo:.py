62af1667bc6a5d15ab947913ebdcb10ae8eacd29
Andreasuarez23 <130806087+Andreasuarez23@users.noreply.github.com>
2024-05-05 12:42:03 -0300

    otros!!

---
+++ class Dispositivo:.py
@@ -0,0 +1,22 @@
+#Actividad 1 
+
+#Formula topologia malla :
+#n(n-1)/2
+
+"""Entoces para 6 dispositivos 
+6*(6-1)/2
+6*5 = 30/2 = 15 
+Respuesta : Se necesitaria 15 cables """
+
+"n-1 : 5 puertos para cada dispositivo"
+
+
+
+
+
+
+
+
+    
+    
+    
